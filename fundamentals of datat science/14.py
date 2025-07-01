2cbc5ff890eb9d712bf959aea056f99560d884d4
prasanth-192324099 <guruprasanthbabud4099.sse@saveetha.com>
2025-07-01 13:57:23 +0530

    Delete fundamentals of datat science directory

--- fundamentals of datat science/14.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from collections import Counter
-
-data = {'age': [25, 30, 22, 30, 28, 25, 35, 30, 22, 25]}
-df = pd.DataFrame(data)
-
-freq_dist = df['age'].value_counts().sort_index()
-print("Frequency distribution of ages:")
-print(freq_dist)
--- fundamentals of datat science/15.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-likes_data = {'likes': [5, 10, 5, 20, 10, 10, 5, 15, 20, 5]}
-df = pd.DataFrame(likes_data)
-
-frequency = df['likes'].value_counts().sort_index()
-print("Frequency of likes:")
-print(frequency)
